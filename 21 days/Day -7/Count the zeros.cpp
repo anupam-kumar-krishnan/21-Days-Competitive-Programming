cee79478234ad1c58dbbc0e4f7e0ed52ef947edb
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2020-10-11 20:10:16 +0530

    Create Count the zeros.cpp

---
+++ 21 days/Day -7/Count the zeros.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{   
+public:
+    int countZeroes(int arr[], int n) 
+    {
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {cnt++;}
+        }
+    return cnt;    
+        
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countZeroes(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
