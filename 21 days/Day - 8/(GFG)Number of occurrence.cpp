ea2318d717f24369b57c5aeb052216bf9e3709b1
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2020-10-12 23:25:53 +0530

    Update (GFG)Number of occurrence.cpp

--- 21 days/Day - 8/(GFG)Number of occurrence.cpp
+++ 21 days/Day - 8/(GFG)Number of occurrence.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class Solution{
 public:	
 
-	int count(int arr[], int n, int x) {
-	
-	    int res = 0; 
-    for (int i=0; i<n; i++) 
-        if (x == arr[i]) 
-          res++; 
-    return res; 
+	int count(int arr[], int n, int x)
+	{
+	  int res = 0; 
+          for (int i=0; i<n; i++) 
+          if(x == arr[i]) 
+             res++; 
+          return res; 
 	}
 };
 
