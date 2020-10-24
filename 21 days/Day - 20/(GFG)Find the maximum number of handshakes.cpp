e94d79a9682963b8c2304371e93f1fdec4747677
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2020-10-24 23:27:51 +0530

    Create (GFG)Find the maximum number of handshakes.cpp

---
+++ 21 days/Day - 20/(GFG)Find the maximum number of handshakes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ {
+	int test;
+	cin >> test;
+	
+	while(test--){
+	    long long int n;
+	    cin >> n;
+	    
+	    cout << (n*(n-1))/2 << endl;
+	}
+	
+	return 0;
+}
